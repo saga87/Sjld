9c8b6d6dc5ec7d91893cb4104f3cb586e61eeaaf
sjc <530795292@qq.com>
2018-05-28 09:43:10 +0800

    添加数据字典实体

---
+++ src/main/module/com/wkrj/basic/dataDictionary/bean/WkrjDataDictionary.java
@@ -0,0 +1,74 @@
+﻿package com.wkrj.basic.dataDictionary.bean;
+
+/**
+ * 数据字典表 对应数据库表：wkrj_sys_data_dictionary
+ * 
+ * @author sjc
+ * 
+ */
+public class WkrjDataDictionary {
+
+	private String id;
+	private String typecode;
+	private String typename;
+	private String typeid;
+	private String typeparentid;
+	private String value;
+	private String notes;
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTypecode() {
+		return typecode;
+	}
+
+	public void setTypecode(String typecode) {
+		this.typecode = typecode;
+	}
+
+	public String getTypename() {
+		return typename;
+	}
+
+	public void setTypename(String typename) {
+		this.typename = typename;
+	}
+
+	public String getTypeid() {
+		return typeid;
+	}
+
+	public void setTypeid(String typeid) {
+		this.typeid = typeid;
+	}
+
+	public String getTypeparentid() {
+		return typeparentid;
+	}
+
+	public void setTypeparentid(String typeparentid) {
+		this.typeparentid = typeparentid;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+}
