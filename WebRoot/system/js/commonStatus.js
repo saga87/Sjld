1e1a3e0960626d85c2a8738e4fec0cefa3969fc9
sjc <530795292@qq.com>
2018-05-30 14:02:38 +0800

    提取共同js

---
+++ WebRoot/system/js/commonStatus.js
@@ -0,0 +1,92 @@
+function print(event_id){
+    var LODOP = getLodop();
+    if(LODOP!=null){
+            var first="";
+               $.ajax({
+                   type:'POST',
+                   url:'eventReportWf/WkrjEventReportWf/getPrintPage?event_id='+event_id,
+                   //data: {event_id:event_id},
+                   async:false,
+                   success:function(data){
+                       first=data;                                  
+                   }  
+               });
+               LODOP.PRINT_INIT("打印");                       
+            LODOP.ADD_PRINT_HTM(50, 60, "90%", "90%", first);
+            LODOP.PREVIEW();                         
+    }
+}
+
+function displayOrNot(rowdata, rowindex, value) {
+    var temp = "";
+    if (rowdata.user_secret == "隐藏用户信息") {
+        temp = '隐藏';
+    } else {
+        temp = value;
+    }
+    return temp;
+}
+
+function secretTrans(rowdata, rowindex, value) {
+    var temp = "";
+    if (value == "1") {
+        temp = '是';
+    } else if (value == "0") {
+        temp = "否";
+    } else {
+        temp = "";
+    }
+    return temp;
+}
+
+function typeTrans(rowdata, rowindex, value) {
+    var temp = "";
+    if (value == "1") {
+        temp = '完成';
+    } else if (value == "2") {
+        temp = "交办";
+    } else if (value == "3") {
+        temp = "转办";
+    } else if (value == "4") {
+        temp = "未完成";
+    }
+    return temp;
+}
+
+function natureTrans(rowdata, rowindex, value) {
+    var temp = "";
+    if (value == "201") {
+        temp = '咨询';
+    } else if (value == "202") {
+        temp = "建议";
+    } else if (value == "203") {
+        temp = "投诉";
+    } else if (value == "204") {
+        temp = "举报";
+    } else if (value == "205") {
+        temp = "求助";
+    }
+    return temp;
+}
+
+function zhuanbanTrans(rowdata, rowindex, value) {
+    var temp = "";
+    if (value == "1") {
+        temp = '是';
+    } else if (value == "0") {
+        temp = "<span style=\"color:red\">否</span>";
+    } else {
+        temp = "";
+    }
+    return temp;
+}
+
+function statusTrans(rowdata, rowindex, value) {
+    if (rowdata.qianshou_status == 1) {
+        return "处理中";
+    } else if (rowdata.qianshou_status == 0) {
+        return "新受理";
+    } else if (rowdata.qianshou_status == 2) {
+        return "处理完毕待评价";
+    }
+}
